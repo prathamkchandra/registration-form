c7896188840f92d1aa6bdf6b178370a2904c05f5
Pratham k <146327009+prathamkchandra@users.noreply.github.com>
2024-12-02 21:56:08 +0530

    Update process.php

--- process.php
+++ process.php
@@ -1,17 +1,28 @@
 <?php
+  
+ // Database connection parameters
+$host = 'sql207.infinityfree.com'; // Database host
+$db = 'if0_37808415_registration_form'; // Database name
+$user = 'if0_37808415'; // Database username
+$pass = 'WiK6m6CG0ERAP7'; // Database password (set if you have one)
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+// Create connection
+$conn = new mysqli($host, $user,$pass, $db);
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $phone = htmlspecialchars($_POST['phone']);
+    $dob = htmlspecialchars($_POST['dob']);
     $address = htmlspecialchars($_POST['address']);
-
-    echo "<h2>Registration Successful</h2>";
+    
+    echo "<h1>Submitted Details</h1>";
     echo "<p><strong>Name:</strong> $name</p>";
     echo "<p><strong>Email:</strong> $email</p>";
     echo "<p><strong>Phone:</strong> $phone</p>";
+    echo "<p><strong>Date of Birth:</strong> $dob</p>";
     echo "<p><strong>Address:</strong> $address</p>";
-} else {
-    echo "Invalid Request";
-}
+  } else {
+    echo "<p>Invalid Request</p>";-
+  }
 ?>
